5f3e82efdce97134c10129445a55903cad4af91b
num876 <maliknumayr@gmail.com>
2026-07-22 18:37:11 +0100

    fix: dummy app init for build

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -2,18 +2,25 @@ import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 
-if (!getApps().length && process.env.FIREBASE_PROJECT_ID) {
-  try {
+if (!getApps().length) {
+  if (process.env.FIREBASE_PROJECT_ID) {
+    try {
+      initializeApp({
+        credential: cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          // Handle newlines in the private key correctly
+          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        }),
+      });
+    } catch (error) {
+      console.error('Firebase admin initialization error', error);
+    }
+  } else {
+    // Dummy initialization for Next.js build step when env vars are missing
     initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // Handle newlines in the private key correctly
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      }),
+      projectId: 'dummy-project-id'
     });
-  } catch (error) {
-    console.error('Firebase admin initialization error', error);
   }
 }
 
